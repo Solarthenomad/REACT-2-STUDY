e86d49a0ba54b40993546f35265cecd1991187b7
solji <oakikim@ewhain.net>
2023-05-31 14:50:09 +0900

    useState, filter, map

--- components/Expenses/Expenses.js
+++ components/Expenses/Expenses.js
@@ -6,6 +6,7 @@ import { useState } from 'react';
 
 const Expenses = (props) => {
 
+  
     const [filteredYear, setfilteredYear] = useState('2020');
 
   const filterChangeHanlder =(selectedYear) =>{
@@ -27,21 +28,25 @@ const Expenses = (props) => {
         amount={props.items[0].amount}
         date={props.items[0].date}
       />
+
       <ExpenseItem
         title={props.items[1].title}
         amount={props.items[1].amount}
         date={props.items[1].date}
       />
+
       <ExpenseItem
         title={props.items[2].title}
         amount={props.items[2].amount}
         date={props.items[2].date}
       />
+
       <ExpenseItem
         title={props.items[3].title}
         amount={props.items[3].amount}
         date={props.items[3].date}
       />
+
     </Card>
     </div>
   );
